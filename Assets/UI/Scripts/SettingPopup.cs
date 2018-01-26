57f61e6c3436ee0aa3892af05f4554a50ec7c886
Unknown <levsha.elody@gmail.com>
2018-01-26 23:50:02 +0300

    Каркас UI
    Добавлен каркас UI, с возможностью перезагруза

---
+++ Assets/UI/Scripts/SettingPopup.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class SettingPopup : MonoBehaviour {
+
+	//[SerializeField] private GameObject popup; 
+
+	public void Open(){
+		//popup.SetActive(true);
+		gameObject.SetActive(true);
+	}
+
+	public void Close(){
+		//popup.SetActive (false);
+		gameObject.SetActive(false);
+	}
+
+}
