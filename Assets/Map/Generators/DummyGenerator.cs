4eabc84ed3a58cb5a445d0358b835b0c24b511b0
kipar <konovod@gmail.com>
2018-01-26 23:37:15 +0300

    updated dummy generator

--- Assets/Map/Generators/DummyGenerator.cs
+++ Assets/Map/Generators/DummyGenerator.cs
@@ -13,6 +13,10 @@ public class DummyGenerator : AbstractGenerator
         for (int x = 0; x < config.LevelWidth; x++)
             for (int y = 0; y < config.LevelHeight; y++)
                 level.Data[x, y] = backs.GetItem(Random.RandomRange(0, 2) == 0 ? WallType.Wall : WallType.Floor, x, y);
+        level.start = new Vector2(0.5f, 0.5f);
+        level.finish = new Vector2(10.5f, 10.5f);
+        level.Data[0, 0] = backs.GetItem(WallType.Floor, 0, 0);
+        level.Data[10, 10] = backs.GetItem(WallType.Floor, 10, 10);
     }
 
 
