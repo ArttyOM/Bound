b8ac5da5d319dfcadf8f4a2f2977a1118f238276
kipar <konovod@gmail.com>
2018-01-27 21:08:35 +0300

    tower object (untested)

--- Assets/Core/Game.cs
+++ Assets/Core/Game.cs
@@ -10,7 +10,13 @@ public class Game : MonoBehaviour {
     Wizard wizard;
     Warrior warrior;
 
+    public int TotalTowers;
+    public int DestroyedTowers;
 
+    public void TowerBroken()
+    {
+        DestroyedTowers++;
+    }
 
     public void NewGame()
     {
