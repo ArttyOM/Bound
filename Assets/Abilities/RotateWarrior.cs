be22fe2f79697e9d5a4e221387bae413882226ac
kipar <konovod@gmail.com>
2018-01-27 14:39:48 +0300

    added abilities (without actual implementation)

---
+++ Assets/Abilities/RotateWarrior.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateWarrior : AbstractAbility 
+{
+
+    override protected void Execute()
+    {
+        Debug.Log("RotateWarrior");
+    }
+
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
