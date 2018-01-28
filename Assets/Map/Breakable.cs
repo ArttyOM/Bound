fc27a8239a52b6c8e770a3ce76a5b0b6c6f2c5ac
kipar <konovod@gmail.com>
2018-01-28 14:42:22 +0300

    variate forests

---
+++ Assets/Map/Breakable.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Breakable : MonoBehaviour {
+
+
+    public void Kill()
+    {
+        Destroy(this.gameObject);
+    }
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
